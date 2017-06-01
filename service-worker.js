ed3cc0a8a68dc6611376c78b15a7eacc071ec95f
Egor Ponomarev <egorponomarev.petruchcho@gmail.com>
2017-06-02 00:54:49 +0400

    no message

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,7 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install ssssssssssssssssssssssssssss');
+    console.log('[ServiceWorker] Install aaaaaaaaaa');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
